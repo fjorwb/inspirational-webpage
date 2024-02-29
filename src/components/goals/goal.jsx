4fbce175ea11e16783b0ef83fbe4c98975c81922
FJORWB <88600252+fjorwb@users.noreply.github.com>
2024-02-29 12:05:58 -0500

    seting up goals component

--- src/components/goals/goal.jsx
+++ src/components/goals/goal.jsx
@@ -2,8 +2,12 @@ import '../../styles/goal.css'
 
 export function Goal({ goal }) {
   return (
-    <div>
-      <span className='goal_title'>{goal}</span>
+    <div className='goal_container'>
+      <div className='goal_btn_container'>
+        <button className='goal_btn edit_btn'>edit</button>
+        <button className='goal_btn delete_btn'>delete</button>
+      </div>
+      <p className='goal_title'>{goal}</p>
     </div>
   )
 }
