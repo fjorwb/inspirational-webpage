dad4d4a7bbf7ac501863c29d33438bcbb1fc7e87
FJORWB <88600252+fjorwb@users.noreply.github.com>
2024-02-29 11:13:51 -0500

    seting up quote component

--- src/components/quote/quote.jsx
+++ src/components/quote/quote.jsx
@@ -4,7 +4,7 @@ function Quote() {
   const quote = `That's the risk you take if you change: that people you've been involved with won't like the new you. But other people who do will come along. —Lisa Alther`
 
   return (
-    <div>
+    <div className='quotecontainer'>
       <p className='quote'>{quote}</p>
     </div>
   )
