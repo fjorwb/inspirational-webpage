9002e2246115d4f2f58372db378dfdafed3de4bd
FJORWB <88600252+fjorwb@users.noreply.github.com>
2024-02-29 18:04:30 -0500

    seting up goals component

--- src/components/goals/goals_bar.jsx
+++ src/components/goals/goals_bar.jsx
@@ -6,14 +6,19 @@ const goals = { goals: [{ title: 'Goal 1' }, { title: 'Goal 2' }, { title: 'Goal
 
 function GoalsBar() {
   return (
-    <ul>
-      {goals.goals.map((goal, index) => (
-        <Goal
-          key={index}
-          goal={goal.title}
-        />
-      ))}
-    </ul>
+    <div className='goals_bar_container'>
+      <ul>
+        {goals.goals.map((goal, index) => (
+          <Goal
+            key={index}
+            goal={goal.title}
+          />
+        ))}
+      </ul>
+      <div className='goals_bar_btn_container'>
+        <button className='goals_bar_btn'>add</button>
+      </div>
+    </div>
   )
 }
 
